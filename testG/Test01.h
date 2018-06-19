0c76e1f8ce357a765714eed9c47afaf36bd41e4e
FatNotSad <1099674042@qq.com>
2018-06-19 16:58:34 +0800

    123

---
+++ testG/Test01.h
@@ -0,0 +1,13 @@
+//
+//  Test01.h
+//  testG
+//
+//  Created by CaiShaozi on 2018/6/19.
+//  Copyright © 2018年 CaiShaozi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test01 : NSObject
+
+@end
---
+++ testG/Test01.m
@@ -0,0 +1,13 @@
+//
+//  Test01.m
+//  testG
+//
+//  Created by CaiShaozi on 2018/6/19.
+//  Copyright © 2018年 CaiShaozi. All rights reserved.
+//
+
+#import "Test01.h"
+
+@implementation Test01
+
+@end
